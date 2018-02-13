af2295cc92cbd9a936da56b05efac15840e0c19d
inteliedoit <lieverandiver@gmail.com>
2018-02-13 10:00:55 +0800

    first commit

---
+++ app/src/main/java/io/vclassify/inteliedoit/vclass/ui/BasicActivity.kt
@@ -0,0 +1,11 @@
+package io.vclassify.inteliedoit.vclass.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/io/vclassify/inteliedoit/vclass/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.vclassify.inteliedoit.vclass.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.vclassify.inteliedoit.vclass.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
